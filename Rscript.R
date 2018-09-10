ca7968ad69e26f52120b0bf18a499a23e9928490
Spandana Sudalagunta <spandanasudalagunta@Spandanas-MBP.columbus.rr.com>
2018-09-09 20:34:58 -0400

    update of comments

--- Rscript.R
+++ Rscript.R
@@ -7,24 +7,24 @@ mydata <- read.csv(url("http://stat511.cwick.co.nz/homeworks/acs_or.csv"))
 summary(mydata)
 # to find out mean of household from the dataset
 mean(mydata$household)
-# to find out median of alubmin from the dataset
+# to find out median of husband's age from the dataset
 median(mydaya$agehusband)
 
-# to find out mode of age from the dataset
+# to find out mode of wife's age from the dataset
 mode(mydata$age_wife)
 
-# to find out standard devation of alkphos from the dataset
+# to find out standard devation of husband's income from the dataset
 sd(mydata$income_husband)
 
-# to find out standard devation of age from the dataset
+# to find out standard devation of wife's income from the dataset
 sd(mydata$income_wife)
-# to find out range of sgpt from the dataset
+# to find out range of bedrooms from the dataset
 range(mydata$bedrooms)
-#  histogram graph of age from the dataset
+#  histogram graph of household from the dataset
 hist(mydata$household)
-#  histogram graph of alubmin from the dataset
+#  histogram graph of bedrooms from the dataset
 hist(mydata$bedrooms)
-# boxplot graph median of alubmin from the dataset
+# boxplot graph of bedrooms from the dataset
 boxplot(mydata$bedrooms)
 
 
